4900402c1236a031c294eb36acd915da86189795
juliarwalter <ju.rheiwalter@gmail.com>
2023-09-21 22:09:48 +0200

    Docs: Add initial files + setup

---
+++ src/main/java/br/com/fiap/StartupOneFIAP/controller/HelloController.java
@@ -0,0 +1,14 @@
+package br.com.fiap.StartupOneFIAP.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("rota")
+public class HelloController {
+    @GetMapping
+    public String getRota() {
+        return "Listagem de rotas";
+    }
+}
