e56aca7d6f494eb543a4b58024f9278a2279ab8a
juliarwalter <ju.rheiwalter@gmail.com>
2023-09-21 17:21:44 +0200

    Docs: Add initial files + setup

---
+++ src/test/java/br/com/fiap/startupone/StartuponeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.startupone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartuponeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
