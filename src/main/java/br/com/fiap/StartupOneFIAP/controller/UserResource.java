580ad5fd75d885c3e8be867389779ad7c5a33ecf
juliarwalter <ju.rheiwalter@gmail.com>
2023-09-22 14:12:32 +0200

    Docs: Add controller.UserResource class

--- src/main/java/br/com/fiap/StartupOneFIAP/controller/UserResource.java
+++ src/main/java/br/com/fiap/StartupOneFIAP/controller/UserResource.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("ususario")
+@RequestMapping("usuario")
 public class UserResource {
 
     @Autowired
