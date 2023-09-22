b06ad96f2198060ee7c200415bef077aab9c06a9
juliarwalter <ju.rheiwalter@gmail.com>
2023-09-22 18:09:27 +0200

    Docs: Add Test structure

---
+++ src/test/java/br/com/fiap/StartupOneFIAP/model/tests/UserTest.java
@@ -0,0 +1,13 @@
+package br.com.fiap.StartupOneFIAP.model.tests;
+
+import br.com.fiap.StartupOneFIAP.model.utils.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+@DisplayName("")
+public class UserTest extends BaseTest {
+    @Test
+    @DisplayName("C001: Test User API - Insert a user without name: Status 400")
+    void testInsertUserWithoutName(){}
+}
---
+++ src/test/java/br/com/fiap/StartupOneFIAP/model/utils/BaseTest.java
@@ -0,0 +1,9 @@
+package br.com.fiap.StartupOneFIAP.model.utils;
+
+public class BaseTest {
+    protected String url = "https://localhost:8081/api/v1";
+    protected String pathUser = "/user";
+    protected String pathReview = "/review";
+    protected String pathCompany = "/company";
+
+}
